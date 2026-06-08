4697001028399e5e36749cb62dc57893eac93c15
Ivan Stetsenko <i.stetsenko1@gmail.com>
2026-06-08 23:56:51 +0100

    chore: add eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+/** @type {import("eslint").Linter.Config[]} */
+const config = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
+
+export default config;
