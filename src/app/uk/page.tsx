2e328433838693dbe845b87c78a0b88f9667f34b
Ivan Stetsenko <i.stetsenko1@gmail.com>
2026-06-18 18:36:41 +0100

    feat(routes): add /uk mirror route tree

---
+++ src/app/uk/page.tsx
@@ -0,0 +1,5 @@
+import { HomeView } from '@/views/HomeView';
+
+const UkHomePage = () => <HomeView lang="uk" />;
+
+export default UkHomePage;
---
+++ src/app/uk/writing/page.tsx
@@ -0,0 +1,5 @@
+import { WritingIndexView } from '@/views/WritingIndexView';
+
+const UkWritingIndexPage = () => <WritingIndexView lang="uk" />;
+
+export default UkWritingIndexPage;
